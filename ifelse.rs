e08bee2ced8f6efb81e88bf09b6ebf19e360752f
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2024-09-21 06:47:14 -0300

    Add files via upload
    file

---
+++ ifelse.rs
@@ -0,0 +1,15 @@
+//if - else if - else - conditions in Rust
+
+fn main () {
+
+let number = 51;
+
+if number > 50 {
+println!("The number is greater than 50");
+}else if number == 50 {
+println!("The number is equal to 50");
+}else {
+println!("The number is less than 50")
+}
+
+}
