0145920c24cdb16a0d29ad8858bbfdf561bfc459
Helton Xavier Hernandez <52825808+heltonx@users.noreply.github.com>
2024-09-22 19:19:59 -0300

    Add files via upload
    add file

---
+++ loop_iterations.rs
@@ -0,0 +1,17 @@
+fn main(){
+
+let mut count = 0;
+
+loop {
+	println!("This will run indefinitely");
+
+
+	count +=1;
+	if count == 3 {
+		break; //exit loop after "count" iterations
+	}
+
+
+}
+
+}
